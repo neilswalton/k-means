3e89a810069fffb782f65583d0eb4d6ed2626fed
neil <neilswalton@gmail.com>
2018-04-18 00:03:24 -0600

    added runtime

--- code/run.py
+++ code/run.py
@@ -10,7 +10,7 @@
 
 import utils
 import kmeans
-
+import time
 
 
 def main():
@@ -19,14 +19,14 @@ def main():
     """
 
     #Dataset
-    file_name = 'iris.csv'
+    file_name = 'frogs.csv'
     file_path = '../data/'
     full_path = file_path+file_name
     data = utils.csv_to_arrays(full_path)
 
     #Parameters
     k = 3
-    mode = 'standard'
+    mode = 'hammerly'
     iterations = 1000
     tests = 10
     model = kmeans.Kmeans(data, k, mode, iterations)
@@ -34,13 +34,19 @@ def main():
     #Book keeping variables
     total_iterations = 0
     total_dist = 0
+    total_time = 0
     
     print("-----" + mode + "-----")
     
     for i in range(tests):
 
+        start = time.time()
         iters, clusters = model.cluster()
+        stop = time.time()
         error = model.average_error(clusters)
+
+        run_time = stop-start
+        total_time += run_time
 
         #print ("Stopped after " + str(iters) + 
         #    " iterations, with average distance of: " + str(error))
@@ -51,6 +57,7 @@ def main():
 
     print ("average iterations: " + str(total_iterations/tests))
     print ("average distance: " + str(total_dist/tests))
+    print ("average time: " + str(total_time/tests))
 
 if __name__ == '__main__':
     main()
